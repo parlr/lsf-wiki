4d63ac4da936fae979cd02fa4ad2b06941a9888c
Edouard Lopez <contact@edouard-lopez.com>
2018-05-18 21:33:40 +0200

    move code in extension/ directory

---
+++ extension/index.js
@@ -0,0 +1,6 @@
+console.log("LSF is loaded");
+let content = "#mw-content-text";
+let blueLinks = "a:not(.new)";
+let links = document.querySelectorAll(`${content} ${blueLinks}`);
+
+console.log(links);
--- index.js
+++
@@ -1,6 +0,0 @@
-console.log('LSF is loaded');
-let content = '#mw-content-text'
-let blueLinks = 'a:not(.new)'
-let links = document.querySelectorAll(`${content} ${blueLinks}`)
-
-console.log(links);
