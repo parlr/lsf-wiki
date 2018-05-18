aaf27eb9393e7b65535b7c1167ec22cdb758044f
Edouard Lopez <contact@edouard-lopez.com>
2018-05-18 17:34:48 +0200

    select blue links

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 console.log('LSF is loaded');
+let content = '#mw-content-text'
+let blueLinks = 'a:not(.new)'
+let links = document.querySelectorAll(`${content} ${blueLinks}`)
+
+console.log(links);
